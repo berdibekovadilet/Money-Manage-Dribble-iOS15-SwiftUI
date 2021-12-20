f970f8e028e86ca6ff2f81e9bbaeac8557dbc656
Adilet <berdibekovadilet@icloud.com>
2021-12-20 19:57:43 +0300

    Initial Commit

---
+++ Money Manage/Money_ManageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_ManageApp.swift
+//  Money Manage
+//
+//  Created by Adilet Berdibekov on 20.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Money_ManageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
